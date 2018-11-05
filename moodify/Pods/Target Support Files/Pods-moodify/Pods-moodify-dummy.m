eebefd96d46ccc6660241dc0a53f5cb1684b7740
Pawel Sendyk <pawelsendyk@berkeley.edu>
2018-11-04 19:19:12 -0800

    Added Firebase Pods

---
+++ moodify/Pods/Target Support Files/Pods-moodify/Pods-moodify-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_moodify : NSObject
+@end
+@implementation PodsDummy_Pods_moodify
+@end
---
+++ moodify/Pods/Target Support Files/Pods-moodifyTests/Pods-moodifyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_moodifyTests : NSObject
+@end
+@implementation PodsDummy_Pods_moodifyTests
+@end
