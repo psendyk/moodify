5bf224bdbb845737f58be832395c1191481165ef
Stephen Boyle <sboyle9338@gmail.com>
2018-11-07 18:50:23 -0800

    finished rough storyboard

---
+++ moodify/moodify/SpeakerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SpeakerViewController.swift
+//  moodify
+//
+//  Created by Stephen Boyle on 11/7/18.
+//  Copyright © 2018 Pawel Sendyk. All rights reserved.
+//
+
+import UIKit
+
+class SpeakerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
