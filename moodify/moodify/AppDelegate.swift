663ebaf6c27f72eb261d59324b45b0d9ef9437bf
Pawel Sendyk <pawelsendyk@berkeley.edu>
2018-11-09 09:36:40 -0800

    added Pods, fetching the storyboard

--- moodify/moodify/AppDelegate.swift
+++ moodify/moodify/AppDelegate.swift
@@ -11,20 +11,108 @@ import CoreData
 import Firebase
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, SPTSessionManagerDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
 
     var window: UIWindow?
 
+    // SPOTIFY
+    // AUTHORIZATION
+    
+    let SpotifyClientID = "991ca7685e364b6a98c3cab163e01f47"
+    let SpotifyRedirectURL = URL(string: "moodify://spotify-login-callback")!
+    
+    lazy var configuration = SPTConfiguration(
+        clientID: SpotifyClientID,
+        redirectURL: SpotifyRedirectURL
+    )
 
+    // Might have to set up a server later - now running locally
+    lazy var sessionManager: SPTSessionManager = {
+        if let tokenSwapURL = URL(string: "https://yourapp.herokuapp.com/api/token"),
+            let tokenRefreshURL = URL(string: "https://yourapp.herokuapp.com/api/refresh_token") {
+            self.configuration.tokenSwapURL = tokenSwapURL
+            self.configuration.tokenRefreshURL = tokenRefreshURL
+            self.configuration.playURI = ""
+        }
+        let manager = SPTSessionManager(configuration: self.configuration, delegate: self)
+        return manager
+    }()
+    
+    
+ 
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        print("fail", error)
+    }
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        print("renewed", session)
+    }
+    
+    /*
+     func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+     
+     }
+    */
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        self.sessionManager.application(app, open: url, options: options)
+        return true
+    }
+    
+    // REMOTE SETUP
+    
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        print("connected")
+        // Connection was successful, you can begin issuing commands
+        self.appRemote.playerAPI?.delegate = self
+        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+            }
+        })
+    }
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        print("disconnected")
+    }
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        print("failed")
+    }
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        print("player state changed")
+        debugPrint("Track name: %@", playerState.track.name)
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        print("success", session)
+        self.appRemote.connectionParameters.accessToken = session.accessToken
+        self.appRemote.connect()
+    }
+    
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: self.configuration, logLevel: .debug)
+        appRemote.delegate = self
+        return appRemote
+    }()
+
+    
+    // SPOTIFY
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        // The next two lines ask the user to log in - will have to invoke it using a button once created
+        let requestedScopes: SPTScope = [.appRemoteControl]
+        self.sessionManager.initiateSession(with: requestedScopes, options: .default)
+        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        // Disconnect the Spotify App Remote
+        if self.appRemote.isConnected {
+            self.appRemote.disconnect()
+        }
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -38,6 +126,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // Re-connect the Spotify App Remote
+        if let _ = self.appRemote.connectionParameters.accessToken {
+            self.appRemote.connect()
+        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
@@ -47,6 +139,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: - Core Data stack
+    
 
     lazy var persistentContainer: NSPersistentContainer = {
         /*
---
+++ moodify/spotify-token-swap-service/Gemfile
@@ -0,0 +1,11 @@
+source "https://rubygems.org"
+
+gem "rake"
+gem "sinatra"
+gem "sinatra-contrib"
+gem "encrypted_strings"
+gem "activesupport"
+gem "dotenv"
+gem "httparty"
+gem "rspec"
+gem "climate_control"
