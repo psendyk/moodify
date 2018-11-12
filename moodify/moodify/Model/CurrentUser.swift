90d2db3dc39ffc1b8fa813e5e5372dafe9d8d231
Grace Hunter <gracehunter@airbears2-10-142-57-40.airbears2.1918.berkeley.edu>
2018-11-11 20:41:07 -0800

    currentuser updates

--- moodify/moodify/Model/CurrentUser.swift
+++ moodify/moodify/Model/CurrentUser.swift
@@ -16,33 +16,38 @@ class CurrentUser {
     var username: String!
     var playlists: [Playlist]
     var profilePicture: UIImage?
+    var topArtists: [String]
+    var topGenres: [String]
+    var currentMood: String
     
     init() {
         playlists = []
     }
     
     func createPlaylist(mood: String) {
-        tracks = []
-        playlist = Playlist(tracks, id: playlists.count + 1)
+        var tracks = []
+        var playlist = Playlist(tracks, id: playlists.count + 1)
         playlists.append(playlist)
     }
     
-    func getTopTracks() {
-        
+    // gets user's top artists and genres
+    func getUserFavorites() {
+        // make request 
     }
     
-    func filterByMood() {
+    // gets recommendations based on artists, genres, and
+    func getRecommendations() {
         
     }
 }
 
 class Playlist {
 
-    var id: int
+    var id: Int
     var name: String
     var tracks: [Track]
         
-    init(tracks: [Track], id: int) {
+    init(tracks: [Track], id: Int) {
         self.id =
         self.name = "Playlist" + String(id)
         self.tracks = tracks
@@ -59,6 +64,24 @@ class Track {
         self.id = id
         self.name = name
         self.artist = artist
+    }
+}
+
+class Mood {
+    
+    var name: String
+    var danceability: Int
+    var energy: Int
+    var instrumentalness: Int
+    var tempo: Int
+    var valence: Int
+    
+    init(name: String, danceability: Int, energy: Int, instrumentalness: Int, tempo: Int, valence: Int) {
+        self.danceability = danceability
+        self.energy = energy
+        self.instrumentalness = instrumentalness
+        self.tempo = tempo
+        self.valence = valence
     }
 }
 
