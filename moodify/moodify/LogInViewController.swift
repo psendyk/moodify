f8c65fbfde7bd4f7f3bd9f48572c09818c537e96
Pawel Sendyk <pawelsendyk@berkeley.edu>
2018-11-11 17:51:23 -0800

    backend skeleton

---
+++ moodify/moodify/LogInViewController.swift
@@ -0,0 +1,83 @@
+//
+//  LoginViewController.swift
+//  moodify
+//
+//  Created by Stephen Boyle on 11/7/18.
+//  Copyright © 2018 Pawel Sendyk. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, SPTSessionManagerDelegate {
+
+    var spotifyController: SpotifyController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButton(_ sender: Any) {
+        let requestedScopes: SPTScope = [.userReadPrivate]
+        self.sessionManager.initiateSession(with: requestedScopes, options: .default)
+    }
+    
+    // AUTHORIZATION
+    
+    let SpotifyClientID = "991ca7685e364b6a98c3cab163e01f47"
+    let SpotifyRedirectURL = URL(string: "moodify://spotify-login-callback")!
+    
+    lazy var configuration = SPTConfiguration(
+        clientID: SpotifyClientID,
+        redirectURL: SpotifyRedirectURL
+    )
+    
+    // Might have to set up a server later - now running locally
+    lazy var sessionManager: SPTSessionManager = {
+        if let tokenSwapURL = URL(string: "https://moodifyy.herokuapp.com/api/token"),
+            let tokenRefreshURL = URL(string: "https://moodifyy.herokuapp.com/api/refresh_token") {
+            self.configuration.tokenSwapURL = tokenSwapURL
+            self.configuration.tokenRefreshURL = tokenRefreshURL
+            self.configuration.playURI = ""
+        }
+        let manager = SPTSessionManager(configuration: self.configuration, delegate: self)
+        return manager
+    }()
+    
+    
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        debugPrint("success", session)
+        performSegue(withIdentifier: "logInToListen", sender: self)
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        debugPrint("fail", error)
+    }
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        print("renewed", session)
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        self.sessionManager.application(app, open: url, options: options)
+        return true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let playlistViewController = segue.destination as? PlaylistViewController {
+            playlistViewController.spotifyController = spotifyController
+        }
+    }
+
+}
